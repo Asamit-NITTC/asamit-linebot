1d40c4882e6a0e964950cfecccd00eacd9a543cf
shun-harutaro <shun_harutaro@protonmail.com>
2023-09-17 19:58:48 +0900

    フォロー時の関数作成

--- followEvent.js
+++ followEvent.js
@@ -1,8 +1,38 @@
-const followEvent = (client, event) => {
-  return client.replyMessage(event.replyToken, {
-    type: "text",
-    text: "登録ありがとうございます"
-  });
+const { default: axios } = require("axios");
+
+const followEvent = async (client, event) => {
+  const isSuccess = await signUp();
+  if (isSuccess) {
+    return client.replyMessage(event.replyToken, paramSuccess);
+  } else {
+    return client.replyMessage(event.replyToken, paramFailed);
+  }
+}
+
+const signUp = async () => {
+  try {
+    await axios.get("", {
+      headers: {
+        Authorization: "Bearer "
+      }
+    })
+      .catch(e => {
+        throw new Error("Failed to signUp " + e.message);
+    });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const paramSuccess = {
+  type: "text",
+  text: "会員登録に成功しました"
+}
+
+const paramFailed = {
+  type: "text",
+  text: "会員登録に失敗しました"
 }
 
 module.exports = followEvent;
