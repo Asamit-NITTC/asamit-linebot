91e50d5ae415356f941d94394719e8b5266d782c
shun-harutaro <shun_harutaro@protonmail.com>
2023-09-21 07:56:17 +0900

    ボタンテンプレの内容更新

--- buttonTmpWaking.js
+++ buttonTmpWaking.js
@@ -1,15 +1,18 @@
 const buttonTmpWaking = (event, client) => {
+  const timestamp = event.timestamp;
+  const timestampDate = new Date(timestamp);
+  const timeWakeup = timestampDate.getHours()+":"+timestampDate.getMinutes();
   return client.replyMessage(event.replyToken, {
     "type": "template",
     "altText": "this is a buttons template",
     "template": {
       "type": "buttons",
-      "text": "6:03に起床を記録します",
+      "text": timeWakeup + "に起床を記録します",
       "actions": [
         {
-          "type": "message",
+          "type": "uri",
           "label": "はい（目標記入に進む）",
-          "text": "yes"
+          "uri": "https://liff.line.me/1661190118-0GzYyEBV/wakeup"
         },
         {
           "type": "message",
